6efec38643e7e9a004cfe11c63fb87293daa5cce
lewishay <29063729+lewishay@users.noreply.github.com>
2026-02-27 21:43:14 +0000

    Impl basic design and image picker

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,14 @@
-const messageDiv = document.getElementById("message");
+const selectImageButton = document.getElementById("select-button") as HTMLButtonElement;
 
-if (messageDiv) {
-    messageDiv.textContent = "Hello, world!";
-}
+selectImageButton.addEventListener("click", async () => {
+    const tabs = await browser.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+
+    if (!tabs[0]?.id) return;
+
+    await browser.tabs.sendMessage(tabs[0].id, {
+        type: "START_ELEMENT_PICK"
+    });
+});
