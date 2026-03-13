4f23ed5352fbb6a7b7bc882a1843ef9db0c3bed0
lewishay <29063729+lewishay@users.noreply.github.com>
2026-03-13 10:56:42 +0000

    Make use of constants file, remove support for local files

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export type ImageReplacementRule = {
+    id: number;
+    oldSrc: string;
+    newSrc: string;
+};
+
+export type SupportedImage =
+    | { type: "img"; element: HTMLImageElement }
+    | { type: "background"; element: HTMLElement; url: string };
+
+export const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+export const bgImgRegex = /url\(["']?(.*?)["']?\)/
+
+export const pseudos: Array<string> = ["none", "::before", "::after"];
