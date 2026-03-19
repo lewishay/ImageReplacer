72faba900406d5ba140aa65d03d903460e0a0833
lewishay <29063729+lewishay@users.noreply.github.com>
2026-03-19 14:53:49 +0000

    Impl modify rule functionality

--- src/existingRules.ts
+++ src/existingRules.ts
@@ -1,9 +1,12 @@
-import { ImageReplacementRule } from "./constants";
+import { ImageReplacementRule, urlRegex } from "./constants";
+
+let modifyPopup: HTMLDivElement | null = null;
 
 document.addEventListener("DOMContentLoaded", async () => {
     const result = await browser.storage.local.get("imageRules");
     const rules = result.imageRules ?? [];
     await populateTableRows(rules);
+    document.addEventListener("keydown", onKeyDown, true);
 });
 
 async function populateTableRows(rules: ImageReplacementRule[]) {
@@ -47,7 +50,7 @@ async function createRow(table: HTMLElement, rule: ImageReplacementRule) {
     let modifyButton = document.createElement("button");
     modifyButton.classList += "modify-button";
     modifyButton.textContent = "Modify";
-    modifyButton.addEventListener("click", () => modifyRule(rule));
+    modifyButton.addEventListener("click", (e: MouseEvent) => modifyRule(rule, e));
     fourthCol.appendChild(modifyButton);
 
     let deleteButton = document.createElement("button");
@@ -61,8 +64,67 @@ async function createRow(table: HTMLElement, rule: ImageReplacementRule) {
     table.appendChild(row);
 }
 
-function modifyRule(targetRule: ImageReplacementRule) {
+function onKeyDown(e: KeyboardEvent) {
+    if (e.key === "Escape") {
+        modifyPopup?.remove();
+        modifyPopup = null;
+    }
+}
 
+function modifyRule(targetRule: ImageReplacementRule, e: MouseEvent) {
+    document.getElementById("rule-modify-popup")?.remove();
+    modifyPopup = document.createElement("div");
+    modifyPopup.id = "rule-modify-popup";
+    modifyPopup.style.left = `${e.clientX}px`;
+    modifyPopup.style.top = `${e.clientY}px`;
+
+    let h2 = document.createElement("h2");
+    h2.textContent = "Enter the URL of an image file";
+    modifyPopup.appendChild(h2);
+
+    let inputDiv = document.createElement("div");
+    modifyPopup.appendChild(inputDiv);
+
+    let input = document.createElement("input");
+    inputDiv.appendChild(input);
+
+    let buttonsDiv = document.createElement("div");
+    modifyPopup.appendChild(buttonsDiv);
+
+    let confirmButton = document.createElement("button");
+    confirmButton.id = "confirm-button";
+    confirmButton.textContent = "Confirm";
+    confirmButton.addEventListener("click", () => confirmModification(targetRule, input.value));
+    buttonsDiv.appendChild(confirmButton);
+
+    let exitButton = document.createElement("button");
+    exitButton.id = "exit-button";
+    exitButton.textContent = "Exit";
+    exitButton.addEventListener("click", () => document.getElementById("rule-modify-popup")?.remove());
+    buttonsDiv.appendChild(exitButton);
+
+    document.body.appendChild(modifyPopup);
+}
+
+async function confirmModification(targetRule: ImageReplacementRule, newPath: string) {
+    if (newPath.match(urlRegex)) {
+        await browser.runtime.sendMessage({
+            type: "deleteRule",
+            rule: targetRule
+        });
+
+        targetRule.newSrc = newPath;
+
+        await browser.runtime.sendMessage({
+            type: "addRule",
+            rule: targetRule
+        });
+    }
+
+    modifyPopup?.remove();
+    modifyPopup = null;
+
+    window.location.reload();
 }
 
 async function deleteRule(targetRule: ImageReplacementRule) {
@@ -70,4 +132,5 @@ async function deleteRule(targetRule: ImageReplacementRule) {
         type: "deleteRule",
         rule: targetRule
     });
+    window.location.reload();
 }
